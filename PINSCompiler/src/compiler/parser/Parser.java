a24d3400b4fb96ba843ff9bb7ae62d5e3a14d9b6
Tadej Delopst <td7908@student.uni-lj.si>
2023-03-12 11:10:37 +0100

    var_definition and type_definition implemented; started function_definition

--- PINSCompiler/src/compiler/parser/Parser.java
+++ PINSCompiler/src/compiler/parser/Parser.java
@@ -44,7 +44,118 @@ public class Parser {
     }
 
     private void parseSource() {
+        dump("source -> definitions");
+    }
 
+    private void parseDefinitions() {
+        dump("definitions -> definition definitions2");
+        parseDefinition();
+        parseDefinitions2();
+    }
+
+    private void parseDefinitions2() {
+        if (check(OP_SEMICOLON)) {
+            dump("definitions2 -> ; definitions");
+            parseDefinitions();
+        } else {
+            dump("definitions2 -> e");
+        }
+    }
+
+    private void parseDefinition() {
+        if (check(KW_TYP)) {
+            dump("definition -> type_definition");
+            skip();
+            parseTypeDefinition();
+        } else if (check(KW_VAR)) {
+            dump("definition -> variable_definition");
+            skip();
+            parseVariableDefinition();
+        } else if (check(KW_FUN)) {
+            dump("definition -> function_definition");
+            skip();
+            parseFunctionDefinition();
+        } else error();
+    }
+
+    private void parseTypeDefinition() {
+        dump("type_definition -> typ identifier : type");
+        if (check(IDENTIFIER)) {
+            skip();
+            if (check(OP_COLON)) {
+                skip();
+                parseType();
+            } else error();
+        } else error();
+    }
+
+    private void parseType() {
+        if (check(AT_LOGICAL)) {
+            dump("type -> logical");
+            skip();
+        } else if (check(AT_INTEGER)) {
+            dump("type -> integer");
+            skip();
+        } else if (check(AT_STRING)) {
+            dump("type -> string");
+            skip();
+        } else if (check(KW_ARR)) {
+            if (check(OP_LBRACKET)) {
+                skip();
+                if (check(C_INTEGER)) {
+                    skip();
+                    if (check(OP_RBRACKET)) {
+                        dump("type -> arr [ int_const ] type");
+                        parseType();
+                    } else error();
+                } else error();
+            } else error();
+        } else error();
+    }
+
+    private void parseVariableDefinition() {
+        if (check(KW_VAR)) {
+            skip();
+            if (check(IDENTIFIER)) {
+                skip();
+                if (check(OP_COLON)) {
+                    dump("variable_definition -> var identifier : type");
+                    skip();
+                    parseType();
+                } else error();
+            } else error();
+        } else error();
+    }
+
+    private void parseFunctionDefinition() {
+        if (check(KW_FUN)) {
+            skip();
+            if (check(IDENTIFIER)) {
+                skip();
+                if (check(OP_LPARENT)) {
+                    dump("function_definition -> fun identifier ( parameters ) : type = expression");
+                    skip();
+                    parseParameters();
+                    if (check(OP_RPARENT)) {
+                        skip();
+                        if (check(OP_COLON)) {
+                            parseType();
+                            if (check(OP_EQ)) {
+                                skip();
+                                parseExpression();
+                            } else error();
+                        } else error();
+                    } else error();
+                } else error();
+            } else error();
+        } else error();
+    }
+
+    private void parseParameters() {
+
+    }
+
+    private void parseExpression() {
     }
 
 
@@ -58,12 +169,15 @@ public class Parser {
     }
 
     private boolean check(TokenType typeToCheck) {
-        if (pointer < symbols.size()) {
-            return symbols.get(pointer).tokenType == typeToCheck;
+        if (pointer + 1 < symbols.size()) {
+            return symbols.get(pointer + 1).tokenType == typeToCheck;
         }
         return false;
     }
 
+    private void error() {
+        Report.error("Error");
+    }
     private void skip() {
         pointer++;
     }
